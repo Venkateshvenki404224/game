be1222c450a183becddded0e35c9b3754e65323e
M Venkatesh <venkateshvenki404224@gmail.com>
2023-12-11 17:31:18 +0000

    Game is working and launched

--- app.py
+++ app.py
@@ -1,40 +1,32 @@
-
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+from pymongo import MongoClient
+# import os
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///game.db'
-db = SQLAlchemy(app)
+application = app = Flask(__name__)
 
-class Scenario(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String, nullable=False)
-    option_a = db.Column(db.String, nullable=False)
-    option_b = db.Column(db.String, nullable=False)
-    next_a = db.Column(db.String, nullable=True)
-    next_b = db.Column(db.String, nullable=True)
+# MongoDB connection
+# mongo_uri = os.environ.get('MONGO_URI')
+# if not mongo_uri:
+#     raise ValueError("No MONGO_URI set for Flask application")
 
-@app.route('/')
+client = MongoClient("mongodb://harry_potter:Qwertyuiop%40123@mongodb.selfmade.ninja:27017/?authSource=users")
+db = client['harry_potter_wizard_game']
+collection = db['stories']
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    scenario = Scenario.query.get(1)  # Start with the first scenario
-    return render_template('index.html', scenario=scenario)
+    story_title = "The Hidden Room"  # Starting story
+    if request.method == 'POST':
+        user_choice = request.form['choice']
+        next_story = collection.find_one({"title": user_choice})
+        if next_story:
+            story_title = next_story['title']
+        else:
+            story_title = "End" 
+            return render_template('end.html') # End of story sequence
 
-@app.route('/<int:scenario_id>', methods=['POST'])
-def choose_option(scenario_id):
-    chosen_option = request.form.get('option')
-    current_scenario = Scenario.query.get(scenario_id)
-    
-    if chosen_option == 'A':
-        next_scenario_id = current_scenario.next_a
-    else:
-        next_scenario_id = current_scenario.next_b
-    
-    # Check if game is over
-    if next_scenario_id == 'end':
-        return render_template('end.html')
-    
-    next_scenario = Scenario.query.get(int(next_scenario_id))
-    return render_template('index.html', scenario=next_scenario)
+    story = collection.find_one({"title": story_title})
+    return render_template('index.html', story=story)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
